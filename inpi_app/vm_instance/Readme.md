c6e4c5bab065b7347a3abeb93d0b8aa692099b64
bletournel <bletournel@egencia.com>
2022-02-04 14:00:17 +0100

    update

--- inpi_app/vm_instance/Readme.md
+++
@@ -1,5 +0,0 @@
-
-gcloud compute instances create test-bl --image-project=debian-cloud --image-family=debian-10 --metadata-from-file startup-script=startup_script.sh --zone=europe-west1-b --machine-type=g1-small
-
-####  view output 
-sudo journalctl -u google-startup-scripts.service
--- inpi_app/vm_instance/deploy.sh
+++
@@ -1,9 +0,0 @@
-MY_INSTANCE_NAME="my-app-instance"
-ZONE=europe-west1-b
-
-gcloud compute instances create $MY_INSTANCE_NAME \
-    --image-family=debian-9 \
-    --image-project=debian-cloud \
-    --machine-type=g1-small \
-    --metadata-from-file startup-script=startup_script.sh \
-    --zone $ZONE
